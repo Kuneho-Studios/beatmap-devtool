03b01563922a7c7eb119a8dbfd3b58496741d509
Joseph McDonough <josephlmcdonough@gmail.com>
2024-04-10 01:30:52 -0400

    fitting code within the first 80 characters in a line

--- BeatmapEdit.py
+++ BeatmapEdit.py
@@ -1,6 +1,7 @@
 ###
-# Contains the flow for when a user would like to update an existing beatmap. This does the bulk of that work.
-# Fetches the desired song and difficulty beatmap then provides prompts to the user to aid in editing the beatmap.
+# Contains the flow for when a user would like to update an existing beatmap.
+# This does the bulk of that work. Fetches the desired song and difficulty
+# beatmap then provides prompts to the user to aid in editing the beatmap.
 ###
 import json
 
@@ -12,9 +13,10 @@ current_lane_configuration = "Two Lanes Left to Right"
 current_lane_configuration_art = LaneArt.TWO_LANES_RIGHT_LEFT[0][0]
 
 
-# open the existing beatmap for editing. then calls method edit_beatmap_input to loop through the editing/adding process
-# obtain the laneEvents since it will not be changed here.
-# paste it at the end of the editing process to ensure it remains.
+# open the existing beatmap for editing. then calls method edit_beatmap_input
+# to loop through the editing/adding process obtain the laneEvents since it
+# will not be changed here. paste it at the end of the editing process to
+# ensure it remains.
 def edit_beatmap(song_name, song_difficulty):
     with open('beatmaps/' + song_name + "/"
               + song_name + "_" + song_difficulty + ".json",
@@ -23,20 +25,24 @@ def edit_beatmap(song_name, song_difficulty):
         notes = json_data["notes"]
         lane_events = json_data["laneEvents"]
     beatmap_read.close()
-    sorted_notes_filled = sorted(edit_beatmap_input(notes), key=lambda note: (note['startBeat'], note['lane']))
+    sorted_notes = sorted(edit_beatmap_input(notes),
+                          key=lambda note: (note['startBeat'], note['lane']))
 
-    with open('beatmaps/' + song_name + "/" + song_name + "_" + song_difficulty + ".json", "w") as beatmap_write:
-        json.dump({"notes": sorted_notes_filled, "laneEvents": lane_events}, beatmap_write, indent=4)
+    with open('beatmaps/' + song_name + "/" + song_name + "_" + song_difficulty
+              + ".json", "w") as beatmap_write:
+        json.dump({"notes": sorted_notes, "laneEvents": lane_events},
+                  beatmap_write, indent=4)
     beatmap_write.close()
 
-    Util.fancy_print_box("✨ " + song_name + " on " + song_difficulty + " difficulty updated! ✨")
+    Util.fancy_print_box("✨ " + song_name + " on " + song_difficulty
+                         + " difficulty updated! ✨")
 
 
 # obtain the beat for the current note to be added to the beatmap
 def edit_beatmap_input(notes):
-    Util.note_report(current_lane_configuration, current_lane_configuration_art, Util.get_last_beat_difficulty())
-    beat = input(
-        "What is the beat for the note you want to add? ")
+    Util.note_report(current_lane_configuration, current_lane_configuration_art,
+                     Util.get_last_beat_difficulty())
+    beat = input("What is the beat for the note you want to add? ")
 
     if beat.lower() == "exit":
         return ""
@@ -45,14 +51,16 @@ def edit_beatmap_input(notes):
             beat = float(beat)
             lane = set_lane(beat)
             note_data = set_note_data(beat, lane)
-            note_json_object = {"startBeat": beat, "lane": lane, "noteData": note_data}
+            note_json_object = {"startBeat": beat,
+                                "lane": lane,
+                                "noteData": note_data}
             notes.append(note_json_object)
 
             edit_beatmap_input(notes)
         except ValueError:
             print("Either enter a number or 'exit'!")
             edit_beatmap_input(notes)
-    print()
+    print("")
     return notes
 
 
@@ -71,14 +79,17 @@ def set_lane(beat):
         return lane_input
 
 
-# set the note type. if is a "laneSwap" then call methods to determine what to swap to
+# set the note type.
+# if is a "laneSwap" then call methods to determine what to swap to
 def set_note_data(beat, lane):
     print("\nAvailable Note Types:")
     Util.dropdown_for_user_input(Util.note_types_list)
 
-    note_type_input = input("Enter the number of beat " + str(beat) + " lane " + str(lane) + "'s note type: ")
+    note_type_input = input("Enter the number of beat " + str(beat) + " lane "
+                            + str(lane) + "'s note type: ")
 
-    note_type_input = Util.validate_dropdown_input(note_type_input, len(Util.note_types_list))
+    note_type_input = Util.validate_dropdown_input(
+        note_type_input, len(Util.note_types_list))
     if note_type_input is None:
         return set_note_data(beat, lane)
     else:
@@ -88,31 +99,39 @@ def set_note_data(beat, lane):
         return {"noteType": Util.note_types_list[note_type_input - 1]}
 
 
-# given the desire to perform a lane swap, prompt and gather how many lanes the user would like the swap to have
+# given the desire to perform a lane swap
+# prompt and gather how many lanes the user would like the swap to have
 def set_lane_swap():
     print("\nAvailable Lane Sizes:")
     lane_names = [value[0] for value in Util.lane_swap_types_dict.values()]
     Util.dropdown_for_user_input(lane_names)
 
-    lane_swap_lane_count = input("Enter the number of lanes in the new lane configuration? ")
+    lane_swap_lane_count = (
+        input("Enter the number of lanes in the new lane configuration? "))
 
-    lane_swap_lane_count = Util.validate_dropdown_input(lane_swap_lane_count, len(lane_names))
+    lane_swap_lane_count = (
+        Util.validate_dropdown_input(lane_swap_lane_count, len(lane_names)))
     if lane_swap_lane_count is None:
         return set_lane_swap()
     else:
         return set_lane_style(lane_swap_lane_count)
 
 
-# given the desired lane swap count, display the art for the different styles and allow the user to select which one
+# given the desired lane swap count, display the art for the different styles
+# allow the user to select which one
 def set_lane_style(lane_swap_lane_count):
-    lane_type_keys = list(Util.lane_swap_types_dict.get(lane_swap_lane_count)[1].keys())
+    lane_type_keys = (
+        list(Util.lane_swap_types_dict.get(lane_swap_lane_count)[1].keys()))
 
     print("\nAvailable Lane Styles:")
     Util.dropdown_for_user_input(lane_type_keys)
 
-    lane_type_input = input("Enter the " + str(lane_swap_lane_count) + "-lane style: ")
+    lane_type_input = (
+        input("Enter the " + str(lane_swap_lane_count) + "-lane style: "))
 
-    lane_type_input = Util.validate_dropdown_input(lane_type_input, len(lane_type_keys))
+    lane_type_input = (
+        Util.validate_dropdown_input(lane_type_input, len(lane_type_keys)))
+
     if lane_type_input is None:
         return set_lane_style(lane_swap_lane_count)
     else:
@@ -120,13 +139,14 @@ def set_lane_style(lane_swap_lane_count):
         current_lane_configuration = list(lane_type_keys)[lane_type_input - 1]
         current_lane_count = lane_swap_lane_count
 
-        lane_configuration = Util.lane_swap_types_dict.get(lane_swap_lane_count)[1].get(
-            current_lane_configuration)
+        lane_configuration = (Util.lane_swap_types_dict.get(
+                lane_swap_lane_count)[1].get(current_lane_configuration))
 
         return set_lane_variation(lane_configuration)
 
 
-# given the desired lane format, display the art for the different variations and allow the user to select which one
+# given the desired lane format, display the art for the different variations
+# allow the user to select which one
 def set_lane_variation(lane_configuration_list):
     lane_configuration_art_list = []
     for lane_configuration in lane_configuration_list:
@@ -134,13 +154,17 @@ def set_lane_variation(lane_configuration_list):
 
     print("\nAvailable Lane Variations:")
     Util.dropdown_for_user_input(lane_configuration_art_list)
-    lane_configuration_input = input("Enter the number of the lane variations you want to swap to: ")
+    lane_configuration_input = (
+        input("Enter the number of the lane variations you want to swap to: "))
 
-    lane_configuration_input = Util.validate_dropdown_input(lane_configuration_input, len(lane_configuration_art_list))
+    lane_configuration_input = Util.validate_dropdown_input(
+        lane_configuration_input, len(lane_configuration_art_list))
+
     if lane_configuration_input is None:
         return set_lane_variation(lane_configuration_list)
     else:
-        selected_lane_list = lane_configuration_list[lane_configuration_input - 1][1]
+        selected_lane_list = (
+            lane_configuration_list)[lane_configuration_input - 1][1]
 
         lane_changes_list = []
         for i in range(5):
@@ -148,7 +172,7 @@ def set_lane_variation(lane_configuration_list):
                 {"lane": i, "newLanePosition": selected_lane_list[i]})
 
         global current_lane_configuration_art
-        current_lane_configuration_art = lane_configuration_list[lane_configuration_input - 1][0]
+        current_lane_configuration_art = (
+            lane_configuration_list)[lane_configuration_input - 1][0]
 
         return lane_changes_list
-
--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 ###
-# Contains the main method. This is the file that is ran to initiate the script and get the initial user input to
-# determine which flow to take.
+# Contains the main method. This is the file that is run to initiate the script
+# and get the initial user input to determine which flow to take.
 ###
 
 
@@ -9,23 +9,26 @@ import BeatmapEdit
 import Util
 
 
-# display the starting message and call the entry method to determine if editing or creating a beatmap
+# display the starting message and call the entry method
+# determine if editing or creating a beatmap
 def main():
     Util.fancy_print_box("✨ Welcome To Project Radiance's Beatmap Dev Tool ✨")
     get_user_purpose()
 
 
-# gets initial user input. if create, then create a new beatmap. if edit, then identify what to edit.
+# gets initial user input. if 'create', then create a new beatmap.
+# if 'edit', then identify what to edit.
 def get_user_purpose():
     create_or_edit = input(
-        "Would you like to create a new beatmap or edit an existing one? (create/edit) ")
+        "Would you like to create a new beatmap or edit an existing one? ")
     if create_or_edit.lower() == "create" or create_or_edit.lower() == "c":
         BeatmapCreation.get_user_input()
     elif create_or_edit.lower() == "edit" or create_or_edit.lower() == "e":
         name, difficulty = get_beatmap()
         BeatmapEdit.edit_beatmap(name, difficulty)
     elif create_or_edit.lower() == "exit":
-        Util.fancy_print_box("💛 Thanks for using Project Radiance's Beatmap Dev Tool 💛")
+        Util.fancy_print_box(
+            "💛 Thanks for using Project Radiance's Beatmap Dev Tool 💛")
         return ""
     else:
         print("Please only enter 'create' or 'edit'")
@@ -38,13 +41,16 @@ def get_beatmap():
     song_list = Util.get_stored_songs()
 
     if len(song_list) == 0:
-        Util.fancy_print_box("⚠ There are no beatmaps present. Please create one first ⚠")
+        Util.fancy_print_box(
+            "⚠ There are no beatmaps present. Please create one first ⚠")
         get_user_purpose()
     else:
         song = Util.input_stored_songs(song_list)
-        difficulty_list = Util.extract_and_sort_difficulties(Util.get_stored_difficulties(song))
+        difficulty_list = Util.extract_and_sort_difficulties(
+            Util.get_stored_difficulties(song))
         difficulty = str(Util.input_stored_difficulties(difficulty_list))
-        Util.fancy_print_box("✨ Editing \"" + Util.get_song_name(str(song)) + "\" on difficulty " + difficulty + " ✨")
+        Util.fancy_print_box("✨ Editing \"" + Util.get_song_name(str(song))
+                             + "\" on difficulty " + difficulty + " ✨")
         print("Type 'exit' to leave editor")
         return song, difficulty
 
